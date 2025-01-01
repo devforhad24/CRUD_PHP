eb2c2f62af1fb7ec19a9f79977346ffe4d2acf5c
Riyad <developerforhad24@gmail.com>
2025-01-01 17:47:48 +0600

    Basic Setup (index.php)

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+ </section>
+<section class="footeroption">
+ <p>© <?php echo date("Y"); ?> Developed By: JavaPhp Coder</p>
+  <h2><?php echo "www.trainingwithliveproject.com"; ?></h2>
+ </section>
+</div>
+</body>
+</html>
